ff8b93887de831dedb475f9015de9501d3c755f4
flakster <carlos.santamaria@gmail.com>
2020-02-12 16:53:33 -0500

    add bubble_sort_by method

--- bubble.rb
+++ bubble.rb
@@ -1,14 +1,35 @@
 #!/usr/bin/ruby
 
 def bubble_sort(arr)
-    (arr.length - 1).times do |x|
+  swapped=true
+  while swapped
+    swapped=false
         arr.each_with_index do |val, id|
             if arr[id + 1] != nil && arr[id] > arr[id + 1]
                 arr[id], arr[id + 1] = arr[id + 1], arr[id]
+                swapped=true
             end
         end
     end
     return arr
 end
 
-print bubble_sort([2, 5, 1, 7, 4])
+def bubble_sort_by(arr)
+  swapped=false
+  arr.each_with_index do |val, id|
+    if arr[id + 1] != nil
+      yield(arr[id], arr[id + 1])
+    end
+  end
+  return arr
+end
+
+#print bubble_sort([2, 5, 1, 7, 0])
+
+bubble_sort_by([3, 2, 1]) do |a, b|
+  if a > b
+    
+  else
+    
+  end
+end
