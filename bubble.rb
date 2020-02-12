27e0ac3b73a6f2214796c270a7038833627c3e1b
Barak <barakbenhamu@gmail.com>
2020-02-12 15:25:10 -0600

    Added bubble_sort method

--- bubble.rb
+++ bubble.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/ruby
 
+def bubble_sort(arr)
+    (arr.length - 1).times do |x|
+        arr.each_with_index do |val, id|
+            if arr[id + 1] != nil && arr[id] > arr[id + 1]
+                arr[id], arr[id + 1] = arr[id + 1], arr[id]
+            end
+        end
+    end
+    return arr
+end
+
+print bubble_sort([2, 5, 1, 7, 4])
