3a5a8291d351b123c94bdeaaccd5ffab02fe9c33
Barak <barakbenhamu@gmail.com>
2020-02-12 16:38:42 -0600

    Ran through llinter and fixed errors

--- bubble.rb
+++ bubble.rb
@@ -1,36 +1,36 @@
 #!/usr/bin/ruby
 
 def bubble_sort(arr)
-  swapped=true
+  swapped = true
   while swapped
-    swapped=false
-        arr.each_with_index do |val, id|
-            if arr[id + 1] != nil && arr[id] > arr[id + 1]
-                arr[id], arr[id + 1] = arr[id + 1], arr[id]
-                swapped=true
-            end
-        end
+    swapped = false
+    arr.each_with_index do |_val, id|
+      if !arr[id + 1].nil? && arr[id] > arr[id + 1]
+        arr[id], arr[id + 1] = arr[id + 1], arr[id]
+        swapped = true
+      end
     end
-    return arr
+  end
+  arr
 end
 
 def bubble_sort_by(arr)
-    swapped=true
-    while swapped
-      swapped=false
-          arr.each_with_index do |val, id|
-              if arr[id + 1] != nil
-                result = yield(arr[id], arr[id + 1])
-                if result > 0
-                    arr[id], arr[id + 1] = arr[id + 1], arr[id]
-                    swapped=true
-                end
-              end
-          end
+  swapped = true
+  while swapped
+    swapped = false
+    arr.each_with_index do |_val, id|
+      next if arr[id + 1].nil?
+
+      result = yield(arr[id], arr[id + 1])
+      if result.positive?
+        arr[id], arr[id + 1] = arr[id + 1], arr[id]
+        swapped = true
       end
-      return arr
+    end
+  end
+  arr
 end
 
-print bubble_sort_by(["hi","hello","hey"]) { |left,right|
-    left.length - right.length
+print bubble_sort_by(%w[hi hello hey]) { |left, right|
+  left.length - right.length
 }
