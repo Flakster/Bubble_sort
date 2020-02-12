4b6ce0178b148245822b9ad0265be58e3399e7ad
Barak <barakbenhamu@gmail.com>
2020-02-12 16:04:46 -0600

    Finished bubble_sort_by and tested

--- bubble.rb
+++ bubble.rb
@@ -15,21 +15,22 @@ def bubble_sort(arr)
 end
 
 def bubble_sort_by(arr)
-  swapped=false
-  arr.each_with_index do |val, id|
-    if arr[id + 1] != nil
-      yield(arr[id], arr[id + 1])
-    end
-  end
-  return arr
+    swapped=true
+    while swapped
+      swapped=false
+          arr.each_with_index do |val, id|
+              if arr[id + 1] != nil
+                result = yield(arr[id], arr[id + 1])
+                if result > 0
+                    arr[id], arr[id + 1] = arr[id + 1], arr[id]
+                    swapped=true
+                end
+              end
+          end
+      end
+      return arr
 end
 
-#print bubble_sort([2, 5, 1, 7, 0])
-
-bubble_sort_by([3, 2, 1]) do |a, b|
-  if a > b
-    
-  else
-    
-  end
-end
+print bubble_sort_by(["hi","hello","hey"]) { |left,right|
+    left.length - right.length
+}
